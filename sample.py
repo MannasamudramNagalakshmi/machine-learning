83ce2a7b1a0e1c2de4fa288f4db4e79ca6a70b40
MannasamudramNagalakshmi <mannasamudramnagalakshmi@gmail.com>
2023-04-26 08:30:17 +0530

    sample commit

---
+++ sample.py
@@ -0,0 +1,20 @@
+"""
+jkk
+"""
+
+
+def add(num_1: int, num_2: int) -> int:
+    """_summary_
+
+    Args:
+        a (int): _description_
+        b (int): _description_
+
+    Returns:
+        int: _description_
+    """
+    result = num_1 + num_2
+    return result
+
+
+print(add(1, 2))
